dde249ee5e460ec534882ef2164b140497668084
Paul Morane <PaulMorane@macbook-pro-de-paul.home>
2016-12-13 22:49:30 +0100

    step

--- scripts/main.js
+++ scripts/main.js
@@ -14,30 +14,48 @@ $(document).ready(function(){
 
     var nI = 0,
         oI = 0,
+
         onePerc = window.innerHeight / 200;
 
     $('body').mousewheel(function(event) {
 
-        if (event.deltaFactor >= 100) event.deltaFactor = event.deltaFactor / 40;
+        if (event.deltaFactor >= 10) event.deltaFactor = event.deltaFactor / 40;
         if (event.deltaY <= -1) nI+=event.deltaFactor;
 
         if (nI - oI >= 1.25){
 
             bgTop = parseFloat($('.bcg1').css('top'));
             nbPerc = (nI - oI) / 0.75;
+
             var value = bgTop - (nbPerc * onePerc);
-            $('.bcg1').css('top', value+'px')
+            $('.bcg1').css('top', value+'px');
             oI = nI;
-            console.log(parseFloat($('.bcg1').css('top')), bgTop);
-            
-            if ( parseFloat($('.bcg1').css('top')) <= -(onePerc * 200)){
-                 $('.bcg1').css('top', (onePerc * 200) +'px')
+
+            if ( bgTop <= -(onePerc * 200)){
+                 $('.bcg1').css('top', (onePerc * 200) +'px');
             };
+
+            if (bgTop < 0) {
+                
+                var bgTop2 = parseFloat($('.bcg2').css('top'));
+
+                var value = bgTop2 - (nbPerc * onePerc);
+
+                $('.bcg2').css('top', value+'px');
+               
+                oI = nI;
+                
+            
+                if ( parseFloat($('.bcg2').css('top')) <= -(onePerc * 200)){
+                    $('.bcg2').css('top', (onePerc * 200) +'px');
+                    bgTopMin = 0;
+                };
+            }
             
 
-        };
+        }
 
-        console.log(event.deltaX, event.deltaY, event.deltaFactor, nI);
+        // console.log(event.deltaX, event.deltaY, event.deltaFactor, nI);
 
     });
 });
