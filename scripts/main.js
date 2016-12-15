f2f8e34e3e860683a220417dd3677909185b2c2c
Paul <paul.morane@hetic.net>
2016-12-15 02:19:53 +0100

    entire shop

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,14 @@ $(document).ready(function(){
     var nI = 0, autonI = 1.25,
         oI = 0,
         onePerc = window.innerHeight / 200,
-        meter = 0, blue = 0, yellow = 0, red = 0,
+        meter = 0, blue = 100, yellow = 1000, red = 100,
         dropRed = 1, dropBlue = 5, dropYel = 15,
         breakValue = 60, scrollRate = 1.5,
         autoCounter = 0, manCounter = 0, dropRateCounter = 0,
-        numRate = false, dropNumRate = 20, numDrop = 1, dropNumCounter = 0;
+        numRate = false, dropNumRate = 20, numDrop = 1, dropNumCounter = 0,
+        autoYelp = 1, autoRedp = 0, autoBluep = 0,
+        dropRateYelp = 1, dropRateRedp = 0, dropRateBluep = 0,
+        dropNumYelp = 1, dropNumRedp = 0, dropNumBluep = 0;
     
     
 
@@ -24,8 +27,8 @@ $(document).ready(function(){
     var autoScrollBuy = document.querySelector('.autoscroll-buy');
     autoScrollBuy.addEventListener("click", autoScroll);
 
-    var manualBuy = document.querySelector('.manual-buy');
-    manualBuy.addEventListener("click", upgradeManualScroll);
+    // var manualBuy = document.querySelector('.manual-buy');
+    // manualBuy.addEventListener("click", upgradeManualScroll);
 
     var dropRateBuy = document.querySelector('.dropRate-buy');
     dropRateBuy.addEventListener("click", upDropRate);
@@ -54,16 +57,41 @@ $(document).ready(function(){
     };
 
     function autoScroll() {
-        if (autoCounter < 30) {
-            setInterval(function() {
-                nI += autonI;
-                bgScroll($('.bcg1'), $('.bcg2'));
-                if (nI % 100 === 0) breakBlock();
-            }, 50);
-            autoCounter += 1;
-            $('.autoscroll-counter').text(autoCounter);
+        if (blue >= autoBluep && yellow >= autoYelp && red >= autoRedp && autoCounter < 30) {
+            if (autoCounter < 30) {
+                setInterval(function() {
+                    nI += autonI;
+                    bgScroll($('.bcg1'), $('.bcg2'));
+                    if (nI % 100 === 0) breakBlock();
+                }, 50);
+                autoCounter += 1;
+                $('.autoscroll-counter').text(autoCounter);
 
-            if (autoCounter % 5 ===0) upgradeManualScroll();
+                if (autoCounter % 5 === 0) upgradeManualScroll();
+            }
+            
+            yellow = yellow - autoYelp;
+            blue = blue - autoBluep;
+            red = red - autoRedp;
+            $('.counter-2>span').text(yellow);
+            $('.counter-3>span').text(blue);
+            $('.counter-4>span').text(red);
+
+            autoYelp =  Math.ceil(autoYelp * 1.6);
+            autoBluep = Math.ceil(autoBluep * 1.5);
+            autoRedp =  Math.ceil(autoRedp * 1.4);
+
+            if (autoCounter === 2) autoBluep =+ 1;
+            if (autoCounter === 5) autoRedp =+ 1;
+            if (autoCounter === 30) {
+                $('.auto-yel-price span').text('MAX');
+                $('.auto-blue-price span').text('MAX');
+                $('.auto-red-price span').text('MAX');
+            } else {
+                $('.auto-yel-price span').text(autoYelp);
+                $('.auto-blue-price span').text(autoBluep);
+                $('.auto-red-price span').text(autoRedp);
+            }
         }
     };
 
@@ -97,7 +125,7 @@ $(document).ready(function(){
             if (numRate = true) {
                 var w = Math.floor((Math.random() * 100));
                 if (w <= dropNumRate) {
-                    yellow += numDrop;
+                    red += numDrop;
                 }
             }
             $('.counter-3>span').text(blue);
@@ -107,7 +135,7 @@ $(document).ready(function(){
             if (numRate = true) {
                 var w = Math.floor((Math.random() * 100));
                 if (w <= dropNumRate) {
-                    yellow += numDrop;
+                    red += numDrop;
                 }
             }
             $('.counter-4>span').text(red);
@@ -119,22 +147,78 @@ $(document).ready(function(){
     };
 
     function upDropRate() {
-        if (dropRateCounter < 100) {
-            dropBlue +=1;
-            dropYel +=1;
-            dropRed +=1;
-        dropRateCounter += 1;
-        $('.dropRate-counter').text(dropRateCounter);
+        if (blue >= dropRateBluep && yellow >= dropRateYelp && red >= dropRateRedp && dropRateCounter < 100) {
+            if (dropRateCounter < 100) {
+                dropBlue +=1;
+                dropYel +=1;
+                dropRed +=1;
+                dropRateCounter += 1;
+                $('.dropRate-counter').text(dropRateCounter);
+            }
+
+            yellow = yellow - dropRateYelp;
+            blue = blue - dropRateBluep;
+            red = red - dropRateRedp;
+            $('.counter-2>span').text(yellow);
+            $('.counter-3>span').text(blue);
+            $('.counter-4>span').text(red);
+
+            dropRateYelp =  Math.ceil(dropRateYelp * 1.25);
+            dropRateBluep = Math.ceil(dropRateBluep * 1.15);
+            dropRateRedp =  Math.ceil(dropRateRedp * 1.05);
+
+            if (dropRateYelp > 1000000) dropRateYelp = 999999;
+            if (dropRateBluep > 100000) dropRateBluep = 99999;
+            if (dropRateCounter === 2) dropRateBluep =+ 1;
+            if (dropRateCounter === 5) dropRateRedp =+ 1;
+            if (dropRateCounter === 100) {
+                $('.rate-yel-price span').text('MAX');
+                $('.rate-blue-price span').text('MAX');
+                $('.rate-red-price span').text('MAX');
+            } else {
+                $('.rate-yel-price span').text(dropRateYelp);
+                $('.rate-blue-price span').text(dropRateBluep);
+                $('.rate-red-price span').text(dropRateRedp);
+            }
         }
     }
 
     function upNumDrop() {
-        numRate = true;
-        dropNumRate +=1;
-        dropNumCounter += 1;
-        if (dropNumCounter % 5 === 0) numDrop =+ 1;
-        $('.dropNum-counter').text(dropNumCounter);
-    }
+        if (blue >= dropNumBluep && yellow >= dropNumYelp && red >= dropNumRedp && dropNumCounter < 100) 
+            if(dropNumCounter < 100) {
+                numRate = true;
+                dropNumRate +=1;
+                dropNumCounter += 1;
+                if (dropNumCounter % 5 === 0) numDrop =+ 1;
+                $('.dropNum-counter').text(dropNumCounter);
+            }
+        
+            yellow = yellow - dropNumYelp;
+            blue = blue - dropNumBluep;
+            red = red - dropNumRedp;
+            $('.counter-2>span').text(yellow);
+            $('.counter-3>span').text(blue);
+            $('.counter-4>span').text(red);
+
+            dropNumYelp =  Math.ceil(dropNumYelp * 1.25);
+            dropNumBluep = Math.ceil(dropNumBluep * 1.15);
+            dropNumRedp =  Math.ceil(dropNumRedp * 1.05);
+
+            if (dropNumYelp > 1000000) dropNumYelp = 999999;
+            if (dropNumBluep > 100000) dropNumBluep = 99999;
+            if (dropNumCounter === 2) dropNumBluep =+ 1;
+            if (dropNumCounter === 5) dropNumRedp =+ 1;
+            if (dropNumCounter === 100) {
+                $('.num-yel-price span').text('MAX');
+                $('.num-blue-price span').text('MAX');
+                $('.num-red-price span').text('MAX');
+            } else {
+                $('.num-yel-price span').text(dropNumYelp);
+                $('.num-blue-price span').text(dropNumBluep);
+                $('.num-red-price span').text(dropNumRedp);
+            }
+        }
+
 
 });
 
